f489e06e6139b2279ea1a656d54ec29919090c2f
Manolo Vera <manolo@droiders.com>
2014-06-11 18:39:22 +0200

    updated processor

--- compiler/src/main/java/com/manolovn/android/apt_sample/annotations/processor/DebugProcessor.java
+++ compiler/src/main/java/com/manolovn/android/apt_sample/annotations/processor/DebugProcessor.java
@@ -5,9 +5,15 @@ import com.manolovn.android.apt_sample.annotations.Debug;
 import javax.annotation.processing.AbstractProcessor;
 import javax.annotation.processing.RoundEnvironment;
 import javax.annotation.processing.SupportedAnnotationTypes;
+import javax.annotation.processing.SupportedSourceVersion;
+import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
+import javax.lang.model.element.PackageElement;
 import javax.lang.model.element.TypeElement;
 import javax.tools.Diagnostic;
+import javax.tools.JavaFileObject;
+import java.io.BufferedWriter;
+import java.io.IOException;
 import java.util.Set;
 
 /**
@@ -16,6 +22,7 @@ import java.util.Set;
  * @author manolovn
  */
 @SupportedAnnotationTypes("com.manolovn.android.ap_sample.annotations.Debug")
+@SupportedSourceVersion(SourceVersion.RELEASE_6)
 public class DebugProcessor extends AbstractProcessor {
 
     public DebugProcessor() {
@@ -28,6 +35,25 @@ public class DebugProcessor extends AbstractProcessor {
             Debug debug = elem.getAnnotation(Debug.class);
             String message = "annotation found in " + elem.getSimpleName();
             processingEnv.getMessager().printMessage(Diagnostic.Kind.NOTE, message);
+
+            TypeElement classElement = (TypeElement) elem;
+            PackageElement packageElement = (PackageElement) classElement.getEnclosingElement();
+
+            JavaFileObject jfo = null;
+            try {
+
+                jfo = processingEnv.getFiler().createSourceFile(classElement.getQualifiedName() + "BeanInfo");
+                BufferedWriter bw = new BufferedWriter(jfo.openWriter());
+                bw.append("package ");
+                bw.append(packageElement.getQualifiedName());
+                bw.append(";");
+                bw.newLine();
+                bw.newLine();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
         }
         return true;
     }
