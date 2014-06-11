e6a8ad62efe5a6f6a1f7b0241a5d55d2084e14bd
Manolo Vera <manolo@droiders.com>
2014-06-11 16:28:10 +0200

    updated annotation processor

--- src/main/java/com/manolovn/android/apt_sample/annotations/processor/DebugProcessor.java
+++ src/main/java/com/manolovn/android/apt_sample/annotations/processor/DebugProcessor.java
@@ -1,9 +1,26 @@
 package com.manolovn.android.apt_sample.annotations.processor;
 
+import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.RoundEnvironment;
+import javax.annotation.processing.SupportedAnnotationTypes;
+import javax.lang.model.element.TypeElement;
+import java.util.Set;
+
 /**
  * Debug processor
  *
  * @author manolovn
  */
-public class DebugProcessor {
+@SupportedAnnotationTypes("com.manolovn.android.ap_sample.annotations.Debug")
+public class DebugProcessor extends AbstractProcessor {
+
+    public DebugProcessor() {
+        super();
+    }
+
+    @Override
+    public boolean process(Set<? extends TypeElement> typeElements, RoundEnvironment roundEnvironment) {
+        return true;
+    }
+
 }
