24cfbfc56da7c785582a55e87b1bd365165f265f
Manolo Vera <manolo.vn@gmail.com>
2014-07-01 01:59:05 +0200

    scan method to find generated class

--- compiler/src/main/java/com/manolovn/android/apt_sample/MethodDebug.java
+++ compiler/src/main/java/com/manolovn/android/apt_sample/MethodDebug.java
@@ -1,9 +1,43 @@
 package com.manolovn.android.apt_sample;
 
+import android.util.Log;
+import com.manolovn.android.apt_sample.annotations.processor.DebugProcessor;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 /**
  * MethodDebug
  *
  * @author manolovn
  */
-public class MethodDebug {
+public final class MethodDebug {
+
+    private MethodDebug() {
+        throw new AssertionError("No instances");
+    }
+
+    public static void scan(Object target) {
+        Class<?> targetClass = target.getClass();
+        Log.d("method debug", "target class >>> " + targetClass.getName());
+        try {
+            Class<?> aClass = Class.forName(targetClass + DebugProcessor.SUFFIX);
+            try {
+                Method method = aClass.getMethod("execute");
+                try {
+                    method.invoke(null);
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                } catch (InvocationTargetException e) {
+                    e.printStackTrace();
+                }
+            } catch (NoSuchMethodException e) {
+                e.printStackTrace();
+            }
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            //scan(targetClass.getSuperclass());
+        }
+    }
+
 }
